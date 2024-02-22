4dba4935dedd9c65203b1ec7158d9ef51d4cdffa
Aminz <3411211078.aminuddin.ihsan@gmail.com>
2024-02-22 17:30:02 +0700

    Surya : Bring-up Matrixx

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -11,15 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := Community
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 
 # Device identifier.
-PRODUCT_NAME := evolution_surya
+PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
+
+# Matrixx
+MATRIXX_BUILD_TYPE := Community
+MATRIXX_MAINTAINER := Inuo
+MATRIXX_CHIPSET := SM6150
+MATRIXX_BATTERY := 5160mah
+MATRIXX_DISPLAY := 1080x2400
+
+# Gapps
+WITH_GMS := true
+BUILD_GOOGLE_MESSAGE := true
