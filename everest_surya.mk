6257aeb3725690c97e9093502eedef8986d924ea
myvinz <alfinashta05@gmail.com>
2024-02-25 04:51:24 +0000

    bring up EverestOS

--- everest_surya.mk
+++ everest_surya.mk
@@ -11,27 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common EverestOS stuff.
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
 
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+EVEREST_BUILD_TYPE := UNOFFICIAL
+EVEREST_MAINTAINER := Skyy | HinohArata
 
-# Device identifier.
-PRODUCT_NAME := lineage_surya
+#Extra Stuffs
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_BLUR := true
+TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# Device Identifier
+PRODUCT_NAME := everest_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_GAPPS_ARCH := arm64
 
-# Matrixx
-MATRIXX_BUILD_TYPE := Community
-MATRIXX_MAINTAINER := Inuo
-MATRIXX_CHIPSET := SM6150
-MATRIXX_BATTERY := 5160mah
-MATRIXX_DISPLAY := 1080x2400
-
-# Gapps
-WITH_GMS := true
-BUILD_GOOGLE_MESSAGE := true
